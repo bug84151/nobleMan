6088a01870469bfb8a8ec7f985c30dc5dbec6d28
bug84151 <bug84151@gmail.com>
2024-04-30 21:08:07 +0100

    javascript update

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const heroImg = document.querySelectorAll(".sliderImg");
-window.addEventListener("load", function (e) {
-  let timer = 1;
-    const countIterval = this.setInterval(() => {
-    heroImg[timer].classList.add("animate");
-    timer++;
-    if (timer > 2) {
-      timer = 0
-    }
-  }, 5000);
-});
+let i = 0
+const slideImages = []
+const time = 8000
 
-console.log(heroImg);
+slideImages[0] = "./assets/image1.jpg"
+slideImages[1] = "./assets/image2.jpg";
+slideImages[2] = "./assets/image3.jpg";
 
-heroImg.forEach((img) => {
-    img.addEventListener("transitionend", (e) => {
-        e.target.classList.remove("animate")
-    })
-})
+function changeImg() { 
+  document.slide.src = slideImages[i]
+
+  if (i < slideImages.length - 1) {
+    i++
+  } else {
+    i = 0
+  }
+
+  setTimeout(changeImg,time)
+}
+
+window.addEventListener("load", changeImg);
