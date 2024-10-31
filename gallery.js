e26a2e438692ae0a3e6f9c28088e4cd29c03c64a
bug84151 <bug84151@gmail.com>
2024-10-31 19:07:30 +0100

    gallery view functionality

--- gallery.js
+++ gallery.js
@@ -1,5 +1,27 @@
 const header = document.querySelector("header");
 const nav = document.querySelector(".nav");
+const modal = document.querySelector(".modal-wrapper");
+const activeImg = document.querySelector(".modal img")
+const allGalleryImg = document.querySelectorAll(".col img");
+
+
+// console.log(activeImg.setAttribute());
+// console.log(allGalleryImg);
+
+modal.addEventListener("click", (e) => {
+  if (e.target.classList.contains('visible')) {
+    modal.classList.remove("visible");
+  }
+  
+});
+
+allGalleryImg.forEach((galleryImg) => {
+  galleryImg.addEventListener("click", (e) => {
+    const imgSrc = e.target.attributes.src.nodeValue;
+    modal.classList.add('visible')
+    activeImg.setAttribute("src", `${imgSrc}`);
+  });
+})
 
 const navObserver = new IntersectionObserver(
   function (entries) {
